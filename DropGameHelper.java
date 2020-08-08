38f1bd29e582cbedfdb53f5bedf1e2589ed4fc2d
Jonathan Striblet <jstriblet@gmail.com>
2020-08-07 23:38:03 -0700

    drop game addition

--- DropGameHelper.java
+++ DropGameHelper.java
@@ -15,5 +15,30 @@ public class DropGameHelper {
 	public int numRows() {return numRows;}
 	public int numCols() {return numCols;}
 
-	public boolean isWinner (String p ) -
+	public boolean isWinner (String p ) {
+		for ( int r=0; r<numRows; r++)
+			for ( int c=0; c<numCols; c++)
+				if ( winCheck(p, board[r][c]))
+					return true;
+		return false;
+	}
+
+	private boolean winCheck (String p, DropGamePiece piece) {
+		if ( piece == null)
+			return false;
+		if ( !p.equals(piece.getSymbol()) )
+			return false;
+		Location loc = piece.getLocation();
+		if ( ! isValid(loc) )
+			return false;
+
+		Location left, right, below, above;
+
+		int adjacentCount = 0;
+		left = loc.left();
+		right = loc.right();
+		below = loc.below();
+		above = loc.above();
+
+	}
 }
